3162c4916f2233a5445b1adc1290a029d32d4578
Hernan Leiva <hleiva@hotmail.com>
2025-01-12 23:19:26 +0100

    It's possible to guess one word at a time

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Game {
-    String name;
-    int attempts;
-    String currentWord;
-    int attemptsLeft;
-    boolean isGameInProgress = true;
-    static String[] countryArray;
+    //variables
+    static String name;
+    static int attempts;
+    static String currentWord;
+    static List<Character> hiddenWord = new ArrayList<>();
+    static boolean isGameInProgress = true;
+    static List<String> countryList = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
 
     // ANSI escape-codes for colors
     public static final String ANSI_RESET = "\u001B[0m";
@@ -24,33 +27,88 @@ public class Game {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    //constructor
-    /*public Game (String name, int attempts){
-        this.name = name;
-        this.attempts = attempts;
-    }*/
-
     public static class GameHelper {
         public static void  start(){
-            Scanner scanner = new Scanner(System.in);
+
             GameHelper.print("Wellcome to Hangman!");
             GameHelper.print("What's your name?");
             String playerName = scanner.nextLine();
+            print("Welcome " + playerName + "!");
             GameHelper.print("How many attempts do you need to guess a country?");
-            int neededAttempts = Integer.parseInt(scanner.nextLine());
-            //Game game = new Game(playerName, neededAttempts);
+            attempts = Integer.parseInt(scanner.nextLine());
+            populateCountryList();
+            currentWord = pickACountry();
+            hideCurrentCountry(currentWord);
 
-            GameHelper.populateCountryList();
-        /*do {
+            do {
 
+                playTheGame();
+            } while (isGameInProgress);
+        }
 
-        } while (isGameInProgress);*/
+        public static void playTheGame(){
+            //check if the user got all the letters correctly.
+            //if yes, ask if the player wants to continue.
+            if (attempts > 0 ) {
+                findLetterInWord();
+            } else {
+                // ask if the player wants to continue.
+                // if not, next line
+                isGameInProgress = false;
+            }
+
+        }
+
+        public static boolean checkIfWordIsCorrect(){
+
+            return true;
+        }
+
+        public static void hideCurrentCountry(String country){
+
+            for (int i = 0; i < country.length(); i++) {
+                hiddenWord.add('-');
+            }
+            for (char letter : hiddenWord){
+                printSameLine('-');
+            }
+            print("");
+            print("Enter a letter you think this country has: ");
+        }
+
+        public static void findLetterInWord(){
+            char userLetter = scanner.nextLine().charAt(0);
+
+            boolean success = false;
+            for (int i = 0; i < currentWord.length() ; i++) {
+                if (userLetter == currentWord.charAt(i)){
+                    hiddenWord.set(i, userLetter);
+                    success = true;
+                }
+            }
+            if (success){
+                print("Good guessed. You have " + attempts + " attempts. ");
+            } else {
+                attempts -= 1;
+                print("Unfortunately that letter isn't in that country's name. You have " + attempts + " attempts left. ");
+            }
+            for (char letter : hiddenWord){
+                GameHelper.printSameLine(letter);
+            }
+            print("");
+        }
+
+        public static String pickACountry(){
+            Random randomNo = new Random();
+            int randomInt = randomNo.nextInt(countryList.size());
+            currentWord = countryList.get(randomInt);
+            countryList.remove(randomInt);
+            return currentWord;
         }
 
         public static void populateCountryList() {
             String currentDirectory = System.getProperty("user.dir");
             String filePath = currentDirectory + "/countries.txt";
-            List<String> countryList = new ArrayList<>();
 
             try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
                 String line;
@@ -62,10 +120,7 @@ public class Game {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            countryArray = countryList.toArray(new String[0]);
-            for ( String c : countryArray){
-                GameHelper.print(c);
-            }
+            //countryArray = countryList.toArray(new String[0]);
         }
 
         public static void print (char charToPrint){
@@ -74,6 +129,10 @@ public class Game {
 
         public static void print (String stringToPrint){
             System.out.println(stringToPrint);
+        }
+
+        public static void printSameLine(char charToPrint){
+            System.out.print(charToPrint);
         }
     }
 }
